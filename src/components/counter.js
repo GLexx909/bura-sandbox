07eb785d82d6794263d1499d7abf7fb4074b53cb
Vitaly Simonov <guardian909@rambler.ru>
2020-05-12 18:23:29 +0300

    Функция mapDispatchToProps()

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { bindActionCreators } from "redux";
 import { connect } from 'react-redux';
+import * as actions from '../actions'
 
 const Counter = ({ counter, inc, dec, rnd }) => {
   return (
     <div className="jumbotron">
       <h2>{counter}</h2>
       <button
-        onClick={inc}
+        onClick={dec}
         className="btn btn-primary btn-lg">DEC</button>
       <button
-        onClick={dec}
+        onClick={inc}
         className="btn btn-primary btn-lg">INC</button>
       <button
         onClick={rnd}
@@ -24,4 +26,17 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+  const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+  return {
+    inc,
+    dec,
+    rnd: () => {
+      const randomValue = Math.floor(Math.random()*10);
+      rnd(randomValue)
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
